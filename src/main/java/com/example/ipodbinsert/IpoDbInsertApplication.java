daec6c6200d4fdadfde6f8ca5bf2f894f972433b
Kim Jae Hyuk <rlawogur816@gmail.com>
2024-07-22 14:40:29 +0900

    project 추가

---
+++ src/main/java/com/example/ipodbinsert/IpoDbInsertApplication.java
@@ -0,0 +1,13 @@
+package com.example.ipodbinsert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpoDbInsertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpoDbInsertApplication.class, args);
+    }
+
+}
