ce913ea0b90ed9f11fb0f6d8684f6f42e51b65b8
Kim Jae Hyuk <rlawogur816@gmail.com>
2024-07-22 17:57:23 +0900

    aws lambda 핸들러 환경 구성

--- src/main/java/com/example/ipodbinsert/IpoDbInsertApplication.java
+++ src/main/java/com/example/ipodbinsert/IpoDbInsertApplication.java
@@ -1,27 +1,39 @@
 package com.example.ipodbinsert;
 
 import com.amazonaws.Request;
-import com.amazonaws.handlers.RequestHandler;
-import com.amazonaws.handlers.RequestHandler2;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.util.TimingInfo;
 import com.example.ipodbinsert.aws.LambdaHandler;
 import com.example.ipodbinsert.ipo.constant.IpoConstants;
 import com.example.ipodbinsert.ipo.service.IpoService;
+import com.example.ipodbinsert.util.exception.SaveFileException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import javax.naming.Context;
+import java.io.IOException;
 
 @SpringBootApplication
-public class IpoDbInsertApplication extends RequestHandler2 {
+@RequiredArgsConstructor
+public class IpoDbInsertApplication implements RequestHandler<String, String> {
 
+    public final IpoService ipoService;
+    @Override
+    public String handleRequest(String string, Context context) {
+        try {
+            ipoService.save();
+        } catch (IOException e) {
+            throw new SaveFileException(IpoConstants.MESSAGE_400, e);
+        }
+        return IpoConstants.MESSAGE_200;
+    }
 
     public static void main(String[] args) {
-       ApplicationContext context =  SpringApplication.run(IpoDbInsertApplication.class, args);
-        LambdaHandler lambdaHandler = context.getBean(LambdaHandler.class);
+     SpringApplication.run(IpoDbInsertApplication.class, args);
+
     }
 
 
