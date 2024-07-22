0ab80fc579ab6ebcc00f87424e2a8f8a3fa177b4
Hyuk <rlawogur816@gmail.com>
2024-07-22 23:08:59 +0900

    lambda -> docker container 방식으로 변경 cron schedule

--- src/main/java/com/example/ipodbinsert/IpoDbInsertApplication.java
+++ src/main/java/com/example/ipodbinsert/IpoDbInsertApplication.java
@@ -1,38 +1,15 @@
 package com.example.ipodbinsert;
 
-import com.amazonaws.Request;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.util.TimingInfo;
-import com.example.ipodbinsert.aws.LambdaHandler;
-import com.example.ipodbinsert.ipo.constant.IpoConstants;
-import com.example.ipodbinsert.ipo.service.IpoService;
-import com.example.ipodbinsert.util.exception.SaveFileException;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import java.io.IOException;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@RequiredArgsConstructor
-public class IpoDbInsertApplication implements RequestHandler<String, String> {
-
-    public final IpoService ipoService;
-    @Override
-    public String handleRequest(String string, Context context) {
-        try {
-            ipoService.save();
-        } catch (IOException e) {
-            throw new SaveFileException(IpoConstants.MESSAGE_400, e);
-        }
-        return IpoConstants.MESSAGE_200;
-    }
+@EnableScheduling
+public class IpoDbInsertApplication {
 
     public static void main(String[] args) {
-     SpringApplication.run(IpoDbInsertApplication.class, args);
+      SpringApplication.run(IpoDbInsertApplication.class, args);
 
     }
 
--- src/main/java/com/example/ipodbinsert/ipo/service/impl/IpoServiceImpl.java
+++ src/main/java/com/example/ipodbinsert/ipo/service/impl/IpoServiceImpl.java
@@ -8,11 +8,14 @@ import com.example.ipodbinsert.util.S3.service.impl.S3ServiceImpl;
 import com.example.ipodbinsert.util.csv.CSVReader;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -24,14 +27,23 @@ public class IpoServiceImpl implements IpoService {
     private final CSVReader csvReader;
     private final S3Service s3Service;
 
+    private LocalDate lastRunDate = LocalDate.now().minusDays(3); // 초기값 설정
 
     @Override
     @Transactional
+    @Scheduled(cron = "0 0 23 * * ?") // 매일 23시에 실행
     public void save() throws IOException {
-        s3Service.downloadFile();
-        String filePath =  S3ServiceImpl.saveDir + File.separator + S3ServiceImpl.fileName;
-        List<Ipo> ipoList = csvReader.readCSV(filePath);
-        ipoRepository.saveAll(ipoList);
+        LocalDate today = LocalDate.now();
+        long daysBetween = ChronoUnit.DAYS.between(lastRunDate, today);
+
+        if (daysBetween >= 3) {
+            // 3일이 지났다면 작업 수행
+            s3Service.downloadFile();
+            String filePath = S3ServiceImpl.saveDir + File.separator + S3ServiceImpl.fileName;
+            List<Ipo> ipoList = csvReader.readCSV(filePath);
+            ipoRepository.saveAll(ipoList);
+            lastRunDate = today; // 마지막 실행일 갱신
+        }
     }
 
 }
