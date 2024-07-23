063ec0b93bf19eeda00fce85eb05a66c97376043
Kim Jae Hyuk <rlawogur816@gmail.com>
2024-07-23 13:17:18 +0900

    sam build 시 test 코드 오류 수정

--- src/test/java/com/example/ipodbinsert/IpoDbInsertApplicationTests.java
+++ src/test/java/com/example/ipodbinsert/IpoDbInsertApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.ipodbinsert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class IpoDbInsertApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class IpoDbInsertApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
