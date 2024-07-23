56b51da63101a0d990670016927396704a108efb
Kim Jae Hyuk <rlawogur816@gmail.com>
2024-07-23 15:40:08 +0900

    servlet 오류 수정

--- src/main/java/com/example/ipodbinsert/IpoDbInsertApplication.java
+++ src/main/java/com/example/ipodbinsert/IpoDbInsertApplication.java
@@ -1,24 +1,14 @@
 package com.example.ipodbinsert;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.example.ipodbinsert.ipo.constant.IpoConstants;
-import com.example.ipodbinsert.ipo.service.IpoService;
-import com.example.ipodbinsert.util.exception.SaveFileException;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.io.IOException;
 
 @SpringBootApplication
 @EnableScheduling
-@RequiredArgsConstructor
 public class IpoDbInsertApplication {
-
-
 
 
     public static void main(String[] args) {
