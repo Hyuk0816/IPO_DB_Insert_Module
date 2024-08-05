3d030bbef54fcbd045d84a5da6f3f153445010aa
Kim Jae Hyuk <rlawogur816@gmail.com>
2024-08-05 16:52:07 +0900

    프로젝트 경로 수정 redis 캐싱 처리 취소

---
+++ src/main/java/com/sideprj/ipoAlarm/ipo/service/impl/IpoServiceImpl.java
@@ -0,0 +1,57 @@
+package com.sideprj.ipoAlarm.ipo.service.impl;
+
+import com.sideprj.ipoAlarm.ipo.dto.IpoGetAllDto;
+import com.sideprj.ipoAlarm.ipo.entity.Ipo;
+import com.sideprj.ipoAlarm.ipo.repository.IpoRepository;
+import com.sideprj.ipoAlarm.ipo.service.IpoService;
+import com.sideprj.ipoAlarm.util.S3.service.S3Service;
+import com.sideprj.ipoAlarm.util.S3.service.impl.S3ServiceImpl;
+import com.sideprj.ipoAlarm.util.csv.CSVReader;
+import com.sideprj.ipoAlarm.util.page.PageResponseVo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class IpoServiceImpl implements IpoService {
+
+    private final IpoRepository ipoRepository;
+    private final CSVReader csvReader;
+    private final S3Service s3Service;
+
+    private LocalDate lastRunDate = LocalDate.now().minusDays(3); // 초기값 설정
+
+    private int count = 0;
+
+    @Override
+    @Transactional
+    @Scheduled(cron = "0 0 2 * * ?") // 매일 새벽 3시에 실행 but 2일에 한번 실행 되도록 아래
+    public void save() throws IOException {
+        LocalDate today = LocalDate.now();
+        long daysBetween = ChronoUnit.DAYS.between(lastRunDate, today);
+
+        if (daysBetween >= 2) {
+            // 3일이 지났다면 작업 수행
+            //log DB를 하나 파자
+            s3Service.downloadFile();
+            String filePath = S3ServiceImpl.saveDir + File.separator + S3ServiceImpl.fileName;
+            List<Ipo> ipoList = csvReader.readCSV(filePath);
+            ipoRepository.saveAll(ipoList);
+            count++;
+            log.info("데이터 갱신!" + count + "차");
+            lastRunDate = today; // 마지막 실행일 갱신
+        }
+    }
+
+}
---
+++ src/main/java/com/sideprj/ipoAlarm/util/csv/CSVReader.java
@@ -0,0 +1,63 @@
+package com.sideprj.ipoAlarm.util.csv;
+
+
+import com.sideprj.ipoAlarm.ipo.entity.Ipo;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class CSVReader {
+
+    @Transactional
+    public List<Ipo> readCSV(String filePath) throws IOException {
+
+        Map<String, Ipo> ipoMap = new HashMap<>();
+
+        CSVFormat csvFormat = CSVFormat.EXCEL.builder()
+                .setHeader("name", "confirmPrice", "IPOPrice", "competitionRate", "securities", "start_date", "end_date")
+                .setSkipHeaderRecord(true)
+                .build();
+
+        try (Reader reader = new FileReader(filePath , StandardCharsets.UTF_8);
+             CSVParser csvParser = new CSVParser(reader, csvFormat)) {
+
+            for (CSVRecord record : csvParser) {
+                String name = record.get("name");
+                Ipo ipo = ipoMap.getOrDefault(name, new Ipo());
+                SimpleDateFormat startDateFormatter = new SimpleDateFormat("yyyy.MM.dd HH:mm");
+                SimpleDateFormat endDateFormatter = new SimpleDateFormat("yyyy.MM.dd HH:mm");
+
+                ipo = ipo.builder()
+                        .ipoName(name)
+                        .confirmPrice(record.get("confirmPrice"))
+                        .ipoPrice(record.get("IPOPrice"))
+                        .competitionRate(record.get("competitionRate"))
+                        .securities(record.get("securities"))
+                        .startDate(startDateFormatter.parse(record.get("start_date")))
+                        .endDate(endDateFormatter.parse(record.get("end_date")))
+                        .build();
+
+                ipoMap.put(name, ipo);
+            }
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        return new ArrayList<>(ipoMap.values());
+    }
+
+    }
+
