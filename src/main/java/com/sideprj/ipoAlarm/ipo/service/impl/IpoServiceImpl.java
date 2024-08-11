d1bfaa263b0084ea672fa4a0d071fb8dd65b7196
Hyuk <rlawogur816@gmail.com>
2024-08-11 10:41:26 +0900

    db insert 에러 해결 중 (갱신 시 redis에 갱신 날짜 저장)

--- src/main/java/com/sideprj/ipoAlarm/ipo/service/impl/IpoServiceImpl.java
+++ src/main/java/com/sideprj/ipoAlarm/ipo/service/impl/IpoServiceImpl.java
@@ -6,6 +6,7 @@ import com.sideprj.ipoAlarm.ipo.repository.IpoRepository;
 import com.sideprj.ipoAlarm.ipo.service.IpoService;
 import com.sideprj.ipoAlarm.util.S3.service.S3Service;
 import com.sideprj.ipoAlarm.util.S3.service.impl.S3ServiceImpl;
+import com.sideprj.ipoAlarm.util.converter.DateFormatter;
 import com.sideprj.ipoAlarm.util.csv.CSVReader;
 import com.sideprj.ipoAlarm.util.page.PageResponseVo;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 
@@ -29,6 +31,7 @@ public class IpoServiceImpl implements IpoService {
     private final IpoRepository ipoRepository;
     private final CSVReader csvReader;
     private final S3Service s3Service;
+    private final RedisTemplate<String ,String> redisTemplate;
 
     private LocalDate lastRunDate = LocalDate.now().minusDays(3); // 초기값 설정
 
@@ -40,18 +43,26 @@ public class IpoServiceImpl implements IpoService {
     public void save() throws IOException {
         LocalDate today = LocalDate.now();
         long daysBetween = ChronoUnit.DAYS.between(lastRunDate, today);
-
-        if (daysBetween >= 2) {
-            // 3일이 지났다면 작업 수행
-            //log DB를 하나 파자
-            s3Service.downloadFile();
-            String filePath = S3ServiceImpl.saveDir + File.separator + S3ServiceImpl.fileName;
-            List<Ipo> ipoList = csvReader.readCSV(filePath);
-            ipoRepository.saveAll(ipoList);
-            count++;
-            log.info("데이터 갱신!" + count + "차");
-            lastRunDate = today; // 마지막 실행일 갱신
+        String formattedCreatedAt = DateFormatter.format(LocalDateTime.now());
+        try{
+            if (daysBetween >= 2) {
+                // 3일이 지났다면 작업 수행
+                //log DB를 하나 파자
+                s3Service.downloadFile();
+                String filePath = S3ServiceImpl.saveDir + File.separator + S3ServiceImpl.fileName;
+                List<Ipo> ipoList = csvReader.readCSV(filePath);
+                ipoRepository.saveAll(ipoList);
+                count++;
+                log.info("데이터 저장 경로 : " + filePath);
+                log.info("데이터 갱신!" + count + "차");
+                lastRunDate = today; // 마지막 실행일 갱신
+                redisTemplate.opsForValue().set("refresh", formattedCreatedAt);
+            }
+        }catch (Exception e){
+            log.error(e.getMessage());
         }
+
+
     }
 
 }
